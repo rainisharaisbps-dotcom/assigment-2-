18d3186e075f4126d2cda926fb39298424806f1e
rainisharaisbps-dotcom <rainisharaisbps@gmail.com>
2025-08-11 11:02:07 +0530

    Add files via upload

---
+++ task(1).py
@@ -0,0 +1,7 @@
+num = input("enter number")
+num = int(num)
+result = num /2 
+if result == num //2:
+    print(num,'this number is even')
+else:
+     print(num, 'this nuber is odd')
---
+++ task(2).py
@@ -0,0 +1,4 @@
+total_sum = 0
+for x in range(1,51):
+    total_sum += x
+print("the sum of number between 1 to 50 is ",total_sum)
